517b45e113667fdff024458a5c131603caac4341
Ben Packard <ben.packard@gmail.com>
2014-03-30 11:20:28 -0400

    Initial Commit

---
+++ BENTweetbotHeader/BENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BENAppDelegate.h
+//  BENTweetbotHeader
+//
+//  Created by Ben Packard on 3/30/14.
+//  Copyright (c) 2014 Ben Packard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
